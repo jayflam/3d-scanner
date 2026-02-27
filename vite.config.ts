ed3ebbe2c917b5f492961eb25afd9e945e95e30c
Jason Lam <lwh.jlin@gmail.com>
2026-02-27 15:30:31 -0500

    added allowed hosts for netlify server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['devserver-fe-develop--3d-scanner.netlify.app'],
+  },
   plugins: [
     react({
       babel: {
