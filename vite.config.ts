82cc8f2d20bb64be2ea4ca284f7f221e88f434c0
Jason Lam <lwh.jlin@gmail.com>
2026-02-27 19:40:08 -0500

    added .env

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ['devserver-fe-develop--3d-scanner.netlify.app'],
-    hmr: false,
   },
   plugins: [
     react({
